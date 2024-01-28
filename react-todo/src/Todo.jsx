99a1e2c5265116cbafaead0c57b430f9efe00054
daxi0823 <daxi0823@gmail.com>
2024-01-28 15:40:51 +0900

    add todos

--- react-todo/src/Todo.jsx
+++ react-todo/src/Todo.jsx
@@ -3,6 +3,7 @@ import './Todo.css';
 
 
 export const Todo = () => {
+	const [todoText, setTodoText] = useState("");
 	const [incompleteTodos, setIncompleteTodos] = useState([
 		"TODO1",
 		"TODO2"
@@ -11,11 +12,23 @@ export const Todo = () => {
 		"TODO3",
 		"TODO4"
 	]);
+	const onChangeTodoText = (e) => setTodoText(e.target.value);
+	const onClickAdd = () => {
+		if (todoText === "") return;
+		const newTodos = [...incompleteTodos, todoText];
+		setIncompleteTodos(newTodos);
+		setTodoText("");
+	};
+
 	return (
 		<>
 			<div className="input-area">
-				<input placeholder='TODOを入力'/>
-				<button>追加</button>
+				<input
+					placeholder='TODOを入力'
+					value={todoText}
+					onChange={onChangeTodoText}
+				/>
+				<button onClick={onClickAdd}>追加</button>
 			</div>
 			<div className='incomplete-area'>
 				<p className='title'>未完了のTODO</p>
