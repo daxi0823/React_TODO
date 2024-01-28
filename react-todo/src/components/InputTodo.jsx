b105d1a2a5db4c633d36ccb18d58d1cc114bbffc
daxi0823 <daxi0823@gmail.com>
2024-01-28 17:07:30 +0900

    divide css for component

--- react-todo/src/components/InputTodo.jsx
+++ react-todo/src/components/InputTodo.jsx
@@ -1,13 +1,22 @@
+const style = {
+  backgroundColor: "antiquewhite",
+  width: "400px",
+  height: "30px",
+  margin: "8px",
+  padding: "8px",
+  borderRadius: "8px"
+}
+
 export const InputTodo = (props) => {
 	const { todoText, onChange, onClick } = props;
 	return (
-		<div className="input-area">
-		<input
-			placeholder='TODOを入力'
-			value={todoText}
-			onChange={onChange}
-		/>
-		<button onClick={onClick}>追加</button>
-	</div>
+		<div style={style}>
+			<input
+				placeholder='TODOを入力'
+				value={todoText}
+				onChange={onChange}
+			/>
+			<button onClick={onClick}>追加</button>
+		</div>
 	)
 }
